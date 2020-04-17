c5b375ecf2480d432c918661d5ce815d0caa7274
Lenvin Gonsalves <lenvin@oykuapp.com>
2020-04-18 00:18:01 +0530

    refactor and update readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Presigned URL for file upload (AWS S3)
+
+## How to run ?
+
+`javascript
+git clone https://github.com/98lenvi/s3-presigned.git
+cd ./s3-presigned
+node index
+`
+
+The server will be running at 127.0.0.1:8080 locally. You will need to add the following in the index file
+
+- [ ] YOUR_ACCESS_KEY
+- [ ] YOUR_SECRET_KEY
+- [ ] YOUR_BUCKET_NAME
+
+Support for two types of operation is provided
+
+- Upload
+- Delete
+
+## Upload
+
+the following Endpoint is for upload
+
+`
+GET 127.0.0.1:8080/uploadFile?myKey=''&mimeType''
+`
+### Query parameters
+- **myKey** should be the directory & file name where you would like to save the file in S3.
+- **mimeType** should be of the file that is going to be uploaded, if there's a mismatch, then the upload will fail.
+
+### Response
+The URL will be sent in the response, on which you'd perform a `PUT` call, where the header would be as follows
+`
+headers: { 'Content-Type': 'MIME_TYPE' },
+`
+
+## Delete
+
+the following Endpoint is for delete
+
+`
+DELETE 127.0.0.1:8080/deleteFile?myKey=''
+`
+### Query parameters
+- **myKey** should be the directory & file name where the file you'd want to delete is in S3.
+
+### Response
+The success of operation will be sent in the response, 
--- index.js
+++ index.js
@@ -1,63 +1,56 @@
-var express = require("express");
-var app = express();
-var cors = require('cors')
+let express = require("express");
+let app = express();
+let cors = require("cors");
 const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: "YOUR_ACCESS_KEY",
+  secretAccessKey: "YOUR_SECRET_KEY",
+  signatureVersion: "v4",
+});
+const myBucket = "YOUR_BUCKET_NAME";
+const signedUrlExpireSeconds = 60 * 5;
 
 app.use(cors());
-app.get("/uploadFile", function(req, res) {
-  const s3 = new AWS.S3();
-  AWS.config.update({
-    accessKeyId: "YOUR_ACCESS_KEY",
-    secretAccessKey: "SECRET_KEY",
-    signatureVersion: 'v4', //important
-  });
-  AWS.config.update({ region: "bucket region (not mandatory)" });
-  const myBucket = "your_bucket_name";
-  const myKey = "directory and filename";
-  const signedUrlExpireSeconds = 60 * 5; //expiry duration of signed url
-
-  const url = s3
-  .getSignedUrl('putObject', {
+app.get("/uploadFile", function (req, res) {
+  let myKey = req.data.myKey;
+  let mimeType = req.data.mimeType
+  let params = {
     Bucket: myBucket,
     Key: myKey,
-    ContentType: 'audio/*', //for any content put * (Read about MIME types)
+    ContentType: mimeType,
     Expires: signedUrlExpireSeconds,
-  })
+  }
+  const url = s3.getSignedUrl("putObject",params);
   res.end(url);
 });
 
-app.get("/deleteFile", function(req, res) {
-    const s3 = new AWS.S3();
-    AWS.config.update({
-      accessKeyId: "YOUR_ACCESS_KEY",
-      secretAccessKey: "SECRET_KEY",
-      signatureVersion: 'v4', //important
-    });
-    AWS.config.update({ region: "bucket region (not mandatory)" });
-    const myBucket = "your_bucket_name";
-    const myKey = "directory and filename";
-    var params = {
-        Bucket: myBucket, 
-        Delete: { // required
-          Objects: [ // required
-            {
-              Key: myKey // required
-            },
-          ],
+app.delete("/deleteFile", function (req, res) {
+  let myKey = req.data.myKey;
+  let params = {
+    Bucket: myBucket,
+    Delete: {
+      Objects: [
+        {
+          Key: myKey,
         },
-      };
-      
-      s3.deleteObjects(params, function(err, data) {
-        if (err){ console.log(err, err.stack);
-         res.end(err)} // an error occurred
-        else{     console.log(data); 
-                  res.end('deleted')}          // successful response
-      });
-    })
-  
-var server = app.listen(8080, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at http://%s:%s", host, port);
+      ],
+    },
+  };
+  s3.deleteObjects(params, function (err, data) {
+    if (err) {
+      console.error(err, err.stack);
+      res.end(err);
+    }
+    else {
+      console.log(data);
+      res.end("deleted");
+    }
+  });
 });
 
+let server = app.listen(8080, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
